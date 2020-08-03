a24491185a945792cc5dfb914f1cf893e98ca656
mariamoljames <mariamol.james@gmail.com>
2020-08-03 00:35:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+  
+
+  function countDown(number) {
+      let i=number-1
+    while (i >= 0) {
+      console.log(i);
+      i-=1
+    }
+    console.log(number);
+  } 
+  
